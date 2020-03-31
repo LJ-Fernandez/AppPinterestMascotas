001261d0f07df9388080f49f3d0dfe32162bcc29
Luis Fernandez <lfernandez@macal.cl>
2020-03-31 14:21:38 -0300

    conflicto en reset

--- db/migrate/20200331013236_add_roles_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddRolesToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :admin_role, :boolean
-    add_column :users, :author_role, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_013236) do
+ActiveRecord::Schema.define(version: 2020_03_31_170022) do
 
   create_table "pins", force: :cascade do |t|
     t.string "titulo"
@@ -31,8 +31,8 @@ ActiveRecord::Schema.define(version: 2020_03_31_013236) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.boolean "admin_role"
-    t.boolean "author_role"
+    t.boolean "admin_role", default: false
+    t.boolean "author_role", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
