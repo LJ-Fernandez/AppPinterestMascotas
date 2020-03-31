3e92a0c3a65fdf926eca1684cdea1ea0c3deac5c
Luis Fernandez <lfernandez@macal.cl>
2020-03-31 13:35:16 -0300

    Antes de hacer rollbak

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,5 @@
 class Pin < ApplicationRecord
+    belongs_to :user
     validates :titulo, uniqueness: true
     validates :titulo, :descripcion, :image, presence: true
     validates :descripcion, length: { minimum:200 }
